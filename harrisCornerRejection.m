67999ff1199bc890043a623728b17c530c310afb
david <dav_2709@hotmail.com>
2017-12-14 03:59:15 -0600

    comentarios

--- harrisCornerRejection.m
+++ harrisCornerRejection.m
@@ -9,30 +9,27 @@ function[finalpositions]= harrisCornerRejection(LLA,octavecount,LLKP,minlevel)
                 
                 P=zeros(rows,cols,maxlevel);
                 
-                for centerlevel = minlevel + 1 : maxlevel   
-                    %disp(['at level ' num2str(centerlevel) ' of octave ' num2str(octave)]);
-                    
-                    %find the points at that octave and scale, Find row,col coords.
+                for centerlevel = minlevel + 1 : maxlevel
                     [r,c] = find(KP(:,:,centerlevel));
                     
                     [Dxx, Dxy, Dyy] = findDerivatives(array, centerlevel);
-                    %                     disp(Dxx);
-                    % 2. further outlier rejection
+                    
                     [lengtha,~] = size(r);
-                    %disp(lengtha);
                     for k = 1 : lengtha
                         i = r(k,1);
                         j = c(k,1);
-                        %use harris corner detector
-                        hessian = [Dxx(i,j) Dxy(i,j)
-                            Dxy(i,j) Dyy(i,j)];
+                        
+                        % Calcular la matriz hessiana
+                        hessian = [Dxx(i,j) Dxy(i,j) Dxy(i,j) Dyy(i,j)];
                         eigenvalues = eig(hessian);
+                        
+                        % Solo usar puntos cuya proporcion <= 2
                         if eigenvalues(2) ~= 0
                             ratio = eigenvalues(1)/eigenvalues(2);
                             if ratio < 0
                                 ratio = -ratio;
                             end
-                            %remove all the edges
+                            
                             if abs(ratio) <= 2
                                 P(i,j, centerlevel )=1;
                             end
