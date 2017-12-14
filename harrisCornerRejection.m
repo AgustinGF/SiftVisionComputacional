419bdd681c2d35b4be413d0c0c875385030f26c0
david <dav_2709@hotmail.com>
2017-12-14 03:41:50 -0600

    Cambios a funciones no usadas

--- harrisCornerRejection.m
+++ harrisCornerRejection.m
@@ -1,38 +1,34 @@
-function[finalpositions]= harrisCornerRejection(LLA,octavecount,LLKP,minlevel)
+function[finalpositions]= harrisCornerRejection(LLA,octavecount,LLKP,minlevel,maxlevel)
             
             finalpositions = {};
             
             for octave = 1 : octavecount   
                 array = LLA{octave};
                 KP=LLKP{octave};
-                [rows,cols,maxlevel]=size(KP);
+                [rows,cols,~]=size(KP);
                 
                 P=zeros(rows,cols,maxlevel);
                 
-                for centerlevel = minlevel + 1 : maxlevel   
-                    %disp(['at level ' num2str(centerlevel) ' of octave ' num2str(octave)]);
+                for centerlevel = minlevel + 1 : maxlevel-1  
                     
-                    %find the points at that octave and scale, Find row,col coords.
                     [r,c] = find(KP(:,:,centerlevel));
                     
                     [Dxx, Dxy, Dyy] = findDerivatives(array, centerlevel);
-                    %                     disp(Dxx);
-                    % 2. further outlier rejection
+                    
                     [lengtha,~] = size(r);
-                    %disp(lengtha);
                     for k = 1 : lengtha
                         i = r(k,1);
                         j = c(k,1);
-                        %use harris corner detector
-                        hessian = [Dxx(i,j) Dxy(i,j)
-                            Dxy(i,j) Dyy(i,j)];
+                        
+                        % Calculo de la matriz hessiana
+                        hessian = [Dxx(i,j) Dxy(i,j) Dxy(i,j) Dyy(i,j)];
                         eigenvalues = eig(hessian);
                         if eigenvalues(2) ~= 0
                             ratio = eigenvalues(1)/eigenvalues(2);
                             if ratio < 0
                                 ratio = -ratio;
                             end
-                            %remove all the edges
+                            %Solo usamos el punto cuando el ratio <=2
                             if abs(ratio) <= 2
                                 P(i,j, centerlevel )=1;
                             end
